8e9b6c66c87c2eadf0aaef4072863f2d8bd05855
HerpMR <al17083@shibaura-it.ac.jp>
2018-11-27 22:15:49 +0900

    負  担  軽  減

--- tasch.h
+++ tasch.h
@@ -1,6 +1,32 @@
 #ifndef TASCH_H
 #define TASCH_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// Use this struct for scheduling algorithm
+typedef struct{
+	char name[10];
+	int t_arrival;
+	int t_cost;
+	bool status;
+} task;
+
+// print task what has done
+void printstatus(task done,int time){
+	printf("Task'%s' is done (arr_time = %3d, fin_time = %3d)\n",
+		done.name, done.t_arrival, time);	
+}
+
+// Use for arrival.c & dealtime.c
+void deal(task todo,int *pass){
+	if(*pass < todo.t_arrival)	*pass = todo.t_arrival;
+	*pass += todo.t_cost;
+	todo.status = true;
+	printstatus(todo,*pass);
+}
+
 
 
 #endif
