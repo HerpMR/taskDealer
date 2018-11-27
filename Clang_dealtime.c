848eca28be35491f0390b8f3b490f9d0acb9bcd7
HerpMR <al17083@shibaura-it.ac.jp>
2018-11-27 22:26:34 +0900

    Changed: source code by creating a header file

--- Clang_dealtime.c
+++ Clang_dealtime.c
@@ -2,20 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "tasch.h"	// Original header files
 // #include "stacklib.h"
 // #include "queuelib.h"
-
-typedef struct{
-	char name[10];
-	int t_arrival;
-	int t_cost;
-	bool status;
-} task;
-
-// prototype & variable
-void deal(task,int *);
-void printstatus(task,int);
-void sort(task *,int,int,int);
 
 int main(int argc, char const *argv[]){
 	int N;
@@ -45,27 +34,5 @@ int main(int argc, char const *argv[]){
 	}
 	printf("%d\n",time);
 	return 0;
-}
-
-void sort(task *TASKLIST,int top,int num,int time){
-	for (int i = top; i < num; ++i){
-		if (TASKLIST[i].t_arrival <= time && TASKLIST[top].t_cost > TASKLIST[i].t_cost){
-			task tmp = TASKLIST[top];
-			TASKLIST[top] = TASKLIST[i];
-			TASKLIST[i] = tmp;
-		}
-	}
-}
-
-void deal(task todo,int *pass){
-	if(*pass < todo.t_arrival)	*pass = todo.t_arrival;
-	*pass += todo.t_cost;
-	todo.status = true;
-	printstatus(todo,*pass);
-}
-
-void printstatus(task done,int time){
-	printf("Task'%s' is done (arr_time = %3d, fin_time = %3d)\n",
-		done.name, done.t_arrival, time);
 }
 
