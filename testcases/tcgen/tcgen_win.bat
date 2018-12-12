4f7fc69d6ff8c2392f3e1ed753d3c8963ca57570
HerpMR <al17083@shibaura-it.ac.jp>
2018-12-12 11:16:15 +0900

    Add & Changed: system of auto-compiling & removing

--- testcases/tcgen/tcgen_win.bat
+++ testcases/tcgen/tcgen_win.bat
@@ -5,11 +5,16 @@ cd /d %~dp0
 rem TestCaseGenerator for Windows
 rem from HerpMR
 
+if not exist a.exe (
+	gcc tcgen.c
+)
+a.exe
 for /l %%i in (1,1,4) do (
 	if not exist case_no%%i.txt (
-		tcgen.exe case_no%%i.txt
+		a.exe case_no%%i.txt
 		sleep 0.75
 	)
 )
+rm a.exe
 echo Generation Completed..
 
