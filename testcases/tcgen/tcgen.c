e58bdc6217725d914673969e71a258cdeaaea2aa
HerpMR <al17083@shibaura-it.ac.jp>
2018-12-12 11:13:56 +0900

    Specification changed

--- testcases/tcgen/tcgen.c
+++ testcases/tcgen/tcgen.c
@@ -7,13 +7,12 @@
 int main(int argc, char const *argv[]){
 	srand((unsigned)time(NULL));
 
-	int N = rand() % 16 + 4;
-	int time = rand() % 3;
+	int N = rand() % (12 + 1) + 3;
+	int time = 0;
 	char *str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	FILE *WTO = fopen(argv[1],"w");
 
 	fprintf(WTO,"%d\n",N);
-	time += rand() % 3;
 	for (int i = 1; i <= N; ++i){
 		fprintf(WTO, "%c %d %d\n",str[i - 1],time,rand() % 10 + 1);
 		time += rand() % 7 + 1;
