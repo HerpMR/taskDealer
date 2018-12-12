ae550a935258b6c57ac4bc7b0101b291687a51a8
HerpMR <al17083@shibaura-it.ac.jp>
2018-12-12 11:01:35 +0900

    Not Finished yet

--- Clang_roundrobin.c
+++ Clang_roundrobin.c
@@ -14,8 +14,9 @@ int main(int argc, char const *argv[]){
 	int lim,time = 0;
 	int queuetop = -1;
 	bool available = true;	// 0: false, 1: true
-	task TASKLIST[100];
+	task TASKLIST[300];
 
+	// Input
 	printf("## Scheduler of Round-Robin method\ninput:\n");
 	scanf("%d", &N);
 	for (int i = 0; i < N; ++i){
@@ -24,7 +25,10 @@ int main(int argc, char const *argv[]){
 		TASKLIST[i].status = 1;	// all tasks is executable
 	}
 	scanf(" %d", &lim);
+	// enqueue(&TASKLIST[0]);
+	TASKLIST[0].status = 1;
 
+	// Output
 	printf("\nOutput:\n");
 	robinstream(TASKLIST,N,lim, &time);
 	printf("%d %.2f\n",time,qtime);
@@ -33,34 +37,33 @@ int main(int argc, char const *argv[]){
 
 // Round-Robin system.
 void robinstream(task *TASKLIST,int size,int limit,int *time){
+	int i = 0;
 	int cont = 1;
 	while(cont){
 		cont = 0;
 
-		for (int i = 0; i < size; ++i){
+		// trace and check
+		if(TASKLIST[i % size].status){
 
-			// trace and check
-			if(TASKLIST[i].status){
-
-				if(TASKLIST[i].t_arrival <= *time){
-					if(TASKLIST[i].t_cost <= limit){
-						*time += TASKLIST[i].t_cost;
-						TASKLIST[i].t_cost = 0;
-						TASKLIST[i].status = 0;
-						qtime += (double)(*time - TASKLIST[i].t_arrival) / (double)size;
-						printstatus(TASKLIST[i],*time,limit);
-					}
-					else{
-						TASKLIST[i].t_cost -= limit;
-						*time += limit;
-						printstatus(TASKLIST[i],*time,limit);
-						TASKLIST[i].status = 3;
-					}
+			if(TASKLIST[i % size].t_arrival <= *time){
+				if(TASKLIST[i % size].t_cost <= limit){
+					*time += TASKLIST[i % size].t_cost;
+					TASKLIST[i % size].t_cost = 0;
+					TASKLIST[i % size].status = 0;
+					qtime += (double)(*time - TASKLIST[i % size].t_arrival) / (double)size;
+					printstatus(TASKLIST[i % size],*time,limit);
 				}
-				else timeadjust(TASKLIST[i - 1], &(*time));
+				else{
+					TASKLIST[i % size].t_cost -= limit;
+					*time += limit;
+					printstatus(TASKLIST[i % size],*time,limit);
+					TASKLIST[i % size].status = 3;
+				}
 			}
+			else timeadjust(TASKLIST[(i - 1) % size], &(*time));
 		}
-		for (int i = 0; i < size; ++i)	cont += TASKLIST[i].status;
+		++i;
+		for (int j = 0; j < size; ++j)	cont += TASKLIST[j].status;
 	}
 	return;
 }
