25a7d881afe871b61c39be8066d60f827f00b36e
hervtea <al17083@shibaura-it.ac.jp>
2019-01-07 16:15:16 +0900

    試作

---
+++ testcases/tcgen/tcgen_maclinux.sh
@@ -0,0 +1,22 @@
+
+SCRIPT_DIR=$(dirname $0)
+cd $SCRIPT_DIR
+
+# TestCaseGenerator for Mac, Linux
+# from hervtea
+
+if [ ! -e a.exe ]; then 
+	gcc tcgen.c
+fi
+a.exe
+for i in 1 2 3 4
+do
+	if [ ! -e case_no$i.txt ]; then
+		a.exe case_no$i.txt
+		sleep 0.75
+	fi
+done
+
+rm a.exe
+echo Generation Completed..
+
